b40b6411a86d2cda69868cd9ceb43287547afa9c
URANO Ryoya <urano.works.mail@gmail.com>
2018-11-23 22:11:00 +0900

    `*`演算の実装

--- 9cc.c
+++ 9cc.c
@@ -64,7 +64,7 @@ void tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-' || *p == '*') {
       tokens[i].ty = *p;
       tokens[i].input = p;
       i++;
