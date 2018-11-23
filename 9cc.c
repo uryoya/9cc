f99692f9fc3789c301beed87dcb9124ae8dadd63
URANO Ryoya <urano.works.mail@gmail.com>
2018-11-23 18:25:25 +0900

    再起下降構文解析をする関数を追加

--- 9cc.c
+++ 9cc.c
@@ -16,6 +16,39 @@ typedef struct {
   char *input; // トークン文字列（エラーメッセージ用）
 } Token;
 
+// 抽象構文木のノードの型
+enum {
+  ND_NUM = 256, // 整数のノードの型
+};
+
+typedef struct Node {
+  int ty;           // 演算子かND_NUM
+  struct Node *lhs; // 左辺
+  struct Node *rhs; // 右辺
+  int val;          // tyがND_NUMの場合のみ使う
+} Node;
+
+Node *expr();
+Node *mul();
+Node *term();
+
+// 新しいノードを作成する（二項演算子）
+Node *new_node(int op, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = op;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+// 新しいノードを作成する（数）
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
 // トークナイズした結果のトークン列はこの配列に保存する
 // 100個以上のトークンは来ないものとする
 Token tokens[100];
@@ -52,6 +85,51 @@ void tokenize(char *p) {
 
   tokens[i].ty = TK_EOF;
   tokens[i].input = p;
+}
+
+// 現在着目しているtokensのインデクス
+int pos = 0;
+
+Node *expr() {
+  Node *lhs = mul();
+  if (tokens[pos].ty == '+') {
+    pos++;
+    return new_node('+', lhs, expr());
+  }
+  if (tokens[pos].ty == '-') {
+    pos++;
+    return new_node('-', lhs, expr());
+  }
+  return lhs;
+}
+
+Node *mul() {
+  Node *lhs = term();
+  if (tokens[pos].ty == '*') {
+    pos++;
+    return new_node('*', lhs, mul());
+  }
+  if (tokens[pos].ty == '/') {
+    pos++;
+    return new_node('/', lhs, mul());
+  }
+  return lhs;
+}
+
+Node *term() {
+  if (tokens[pos].ty == TK_NUM)
+    return new_node_num(tokens[pos++].val);
+  if (tokens[pos].ty == '(') {
+    pos++;
+    Node *node = expr();
+    if (tokens[pos].ty != ')')
+      error("開きカッコに対応する閉じカッコがありません: %s",
+          tokens[pos].input);
+    pos++;
+    return node;
+  }
+  error("数値でも開きカッコでもないトークンです: %s",
+      tokens[pos].input);
 }
 
 // エラーを報告するための関数
